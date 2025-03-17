5d8dc58f4414e847b15fae7221681ed82125fd49
Guilherme Marinho <guimars22@gmail.com>
2025-03-17 18:30:53 -0300

    #

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int numero = Integer.valueOf(args[0]);
+        String agenca = args[1];
+        String nome = args[2];
+        double saldo = Double.valueOf(args[3]);
+
+        System.out.println("Por favor, digite o número da Agência");
+        System.out.println(numero);
+        System.out.println("Por favor, digite o sua Agência");
+        System.out.println(agenca);
+        System.out.println("Por favor, digite o seu Nome");
+        System.out.println(nome);
+        System.out.println("Por favor, digite o seu Saldo");
+        System.out.println(saldo);
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma cona em nosso banco, sua agência é " + agenca + ", conta " + numero + " e seu saldo " + saldo + "já está disponível para saque" );
+
+
     }
 }
